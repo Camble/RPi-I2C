d04344e26ff6f7a6f3e98a565952d1d61daca41a
Kenneth Campbell <Kenneth.Campbell@fife.gov.uk>
2017-09-29 11:41:51 +0100

    Tweaks

--- i2c.py
+++ i2c.py
@@ -30,7 +30,7 @@ def configSlave(value):
 def getVoltage():
   data = ""
   for i in range(0,4):
-    data += chr(bus.read_byte(address))
+    data[i] += chr(bus.read_byte(address))
   
   print(data)
 
